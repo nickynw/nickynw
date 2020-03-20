1aa1c39116777497ef6a4a704e237207e2364520
nickynw <nw15347@my.bristol.ac.uk>
2020-03-20 15:24:53 +0000

    Updates

---
+++ precache-manifest.8e0a823524af8c3df0e6242de98e24dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbedaa9983677bbe1b344a84e13607c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6af3d6686ad29a111d6e",
+    "url": "/static/css/main.aed4894c.chunk.css"
+  },
+  {
+    "revision": "d6033d7ca954082313fb",
+    "url": "/static/js/2.85f49f0b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.85f49f0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af3d6686ad29a111d6e",
+    "url": "/static/js/main.92e4dfba.chunk.js"
+  },
+  {
+    "revision": "05c6fc6742dd7be46c92",
+    "url": "/static/js/runtime-main.b60dd34d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d3d8b561d7ea8b515ae8c3f1ec5507c6.js"
+  "/precache-manifest.8e0a823524af8c3df0e6242de98e24dd.js"
 );
 
 self.addEventListener('message', (event) => {
